d651c1b5f9de1d906adb1a67b95867b7c2ab063c
kumar <kumarajay912282@gmail.com>
2026-08-16 13:14:02 +0530

    Add linear search algorithm

---
+++ S1.java
@@ -0,0 +1,18 @@
+public class S1{
+    public static void main(String[] args){
+     int[] arr = {18, 12, 9, 14, 77, 50};
+     int target= 14;
+     System.out.println(linearSerach(arr, target));
+    }
+    static int linearSerach(int[] arr, int target){
+        if(arr.length==0){
+            return -1;
+        }
+        for(int i =0; i<arr.length ;i++){
+            if (target == arr[i]){
+                return i;
+            }
+        }
+        return target;
+    }
+}
