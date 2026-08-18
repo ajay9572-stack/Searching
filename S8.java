98acb70e0d5d8811ee92022d15dde5c0407ef2c8
kumar <kumarajay912282@gmail.com>
2026-08-18 20:31:47 +0530

    Refactor formatting in S8.java for consistency

--- S8.java
+++ S8.java
@@ -1,14 +1,14 @@
 public class S8 {
+    // bineary search 
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 9, 11,  12, 14, 20, 36, 48};
         int target = 36;
-        System.out.println(binerySearch(arr, target));
-        
+        System.out.println(binerySearch(arr, target));    
     }
     static int binerySearch(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
-
+        
         while(start<= end){
             int mid = start + (end - start) / 2;
 
