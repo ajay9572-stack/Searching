d2ba24c74bb0eb42de50d55e1d22c7096e7e3c50
kumar <kumarajay912282@gmail.com>
2026-08-18 20:55:22 +0530

    Refactor binary search implementation in S9.java to return index instead of boolean and improve comments

--- S9.java
+++ S9.java
@@ -1,35 +1,31 @@
 public class S9 {
+    // order agnostic binary search
+    // first they check it is ascending order or descending order then process as previouws
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 9, 11, 12, 14, 20, 36, 48};
         int target = 36;
-
         System.out.println(binarySearch(arr, target));
     }
 
-    static boolean binarySearch(int[] arr, int target) {
-
+    static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 
-        boolean isAsc = arr[start] < arr[end];
+        boolean isAsc = arr[start] < arr[end];         // Check whether the array is in ascending order If arr[start] < arr[end], the array is ascending. Otherwise, it is descending.
 
         while (start <= end) {
-
             int mid = start + (end - start) / 2;
-
             if (arr[mid] == target) {
-                return true;
+                return mid;
             }
 
-            if (isAsc) {
-                // Ascending array
+            if (isAsc) {                          // If the array is ascending
                 if (target < arr[mid]) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
-            } else {
-                // Descending array
+            } else {                          // If the array is descending
                 if (target > arr[mid]) {
                     end = mid - 1;
                 } else {
@@ -37,7 +33,6 @@ public class S9 {
                 }
             }
         }
-
-        return false;
+        return -1;
     }
 }
