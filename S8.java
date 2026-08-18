010f249612e74c94e323f9152212023541d50f4c
kumar <kumarajay912282@gmail.com>
2026-08-18 20:06:11 +0530

    Refactor output statement in S8.java to inline the binary search result

--- S8.java
+++ S8.java
@@ -2,8 +2,7 @@ public class S8 {
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 9, 11,  12, 14, 20, 36, 48};
         int target = 36;
-        int ans = binerySearch(arr, target);
-        System.out.println(ans);
+        System.out.println(binerySearch(arr, target));
         
     }
     static int binerySearch(int[] arr, int target){
